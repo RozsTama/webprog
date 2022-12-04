9460d36601a65dd7e7e11363b82bb00b8a33521d
klaci0327 <klaci03272002@gmail.com>
2022-12-04 13:06:43 +0100

    videóvezérlők hozzáadása

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -1,5 +1,5 @@
 <iframe width="560" height="315" src="https://www.youtube.com/embed/zeBLbr9SrZ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<video width="560" height="315">
+<video width="560" height="315" controls>
     <source src="videos/golya.mp4" type="video/mp4">
 </video>
 <br>
